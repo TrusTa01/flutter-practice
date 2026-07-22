2ab14ebc98358752703b776b24804e6c941af0f5
Yura Borovikov <danylnet65@gmail.com>
2026-07-22 21:51:19 +1000

    ui fixes

--- basics/cycles.dart
+++ basics/cycles.dart
@@ -1,49 +1,49 @@
 // void main() {
 
-//   // От 1 до 10
+  // От 1 до 10
 
-//   // for (var i = 1; i <= 10; i++) {
-//   //   print(i);
-//   // }
+  // for (var i = 1; i <= 10; i++) {
+  //   print(i);
+  // }
 
-//   // От 10 до 1
+  // От 10 до 1
 
-//   // for (var i = 10; i >= 1; i--) {
-//   //   print(i);
-//   // }
+  // for (var i = 10; i >= 1; i--) {
+  //   print(i);
+  // }
 
-//   // От 2 до 20 четные числа
+  // От 2 до 20 четные числа
 
-//   // for (var i = 2; i <= 20; i += 2 ) {
-//   //   print(i);
-//   // }
+  // for (var i = 2; i <= 20; i += 2 ) {
+  //   print(i);
+  // }
 
-//   // Сумма от 1 до 100
+  // Сумма от 1 до 100
 
-//   // var sum = 0;
+  // var sum = 0;
 
-//   // for (var i = 1; i <= 100; i++) {
-//   //   sum += i ;
-//   //   print("Сумма чисел от одного до ста: $sum");
-//   // }
+  // for (var i = 1; i <= 100; i++) {
+  //   sum += i ;
+  //   print("Сумма чисел от одного до ста: $sum");
+  // }
 
-//   // Факториал 7 (1*2*3*4*5*6*7)
+  // Факториал 7 (1*2*3*4*5*6*7)
 
-//   // var sum = 1;
+  // var sum = 1;
 
-//   // for (var i = 1; i <= 7; i++) {
-//   //   sum *= i;
-//   //   print("Факториал числа 7: $sum");
-//   // }
+  // for (var i = 1; i <= 7; i++) {
+  //   sum *= i;
+  //   print("Факториал числа 7: $sum");
+  // }
 
-//   // 2 в степень 10
+  // 2 в степень 10
 
-//   // var result = 1;
+  // var result = 1;
 
-//   // for (var i = 1; i <= 10; i++) {
-//   //   result *= 2;
-//   //   print("2 в степени $i = $result");
-//   // }
+  // for (var i = 1; i <= 10; i++) {
+  //   result *= 2;
+  //   print("2 в степени $i = $result");
+  // }
 
 // }
 
@@ -61,15 +61,15 @@
 
 // void main() {
 //   List<String> words = ['Dart', 'Flutter', 'Programming', 'Mobile', 'Development'];
-  
-//   String longestWord = '';  
+
+//   String longestWord = '';
 
 //   for (var word in words) {
 //     if (word.length > longestWord.length) {
 //       longestWord = word;
 //     }
 //   }
-//   print('Самое длинное слово: $longestWord');  
+//   print('Самое длинное слово: $longestWord');
 // }
 
 
